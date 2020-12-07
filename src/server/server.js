b8d556be08d9f358e29b4a3a6550a3f15927b8c3
Daniel Zurita <danielzurita94@hotmail.com>
2020-12-07 13:02:06 -0600

    Configurar dotenv y nodemon

--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,21 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const { ENV, PORT } = process.env;
 const app = express();
+
+if (ENV === 'development') {
+  console.log('development config');
+}
 
 // tipo de ruta '/', index. '/ssr' ssr. '*' todas.
 app.get('*', (req, res) => {
   res.send({ hello: 'express' });
 });
 
-app.listen(3000, (err) => {
+app.listen(PORT, (err) => {
   if (err) console.log(err);
-  else console.log('server running on port 3000');
+  else console.log(`server running on port ${PORT}`);
 });
