06d351245647a5e37809378b9e72427a25fdbda6
Daniel Zurita <danielzurita94@hotmail.com>
2020-12-07 13:52:01 -0600

    Configuracion hot module replacement

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import webpack from 'webpack';
 
 dotenv.config();
 
@@ -8,6 +9,16 @@ const app = express();
 
 if (ENV === 'development') {
   console.log('development config');
+  const webpackConfig = require('../../webpack.config');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(webpackConfig);
+  // const serverConfig = { port: PORT, hot: true };
+  const { publicPath } = webpackConfig.output;
+  const serverConfig = { serverSideRender: true, publicPath };
+
+  app.use(webpackDevMiddleware(compiler, serverConfig));
+  app.use(webpackHotMiddleware(compiler));
 }
 
 // tipo de ruta '/', index. '/ssr' ssr. '*' todas.
